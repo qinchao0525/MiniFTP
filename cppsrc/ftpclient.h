5cdcab3f16cdd967d62df7bae890f9f7b18e35de
qinchao <qinchao252508@gmail.com>
2017-07-10 20:59:17 +0800

    add function of ftpclient

--- cppsrc/ftpclient.h
+++ cppsrc/ftpclient.h
@@ -3,8 +3,27 @@
 class ftpclient
 {
 private:
+	enum { 
+		SERVER_PORT = 9999,
+		BUFFER_SIZE = 4096
+	};
+	sockadder_in serverChannel;
+	char buffer[BUFFER_SIZE];
+	int serverSocket;
+	int clientSocket;
+	bool isConnect;
+	char name[50];
+
+	bool getFile();
+	bool putFile();
+	bool acknowledge();
+	bool sendRequest(char * instruction);
+	bool connect2Host(const char* hostName);
+	bool getWorkDir();
 
 public:
-
+	ftpClient();
+	~ftpClient();
+	void start();
 }
 #endif /*_FTPCLIENT_H_*/
