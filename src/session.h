a224046f35dc01897e748e8efedd9f47bd0b5d9c
qinchao <qinchao252508@gmail.com>
2017-07-13 16:19:03 +0800

    add defination of session_t

--- src/session.h
+++ src/session.h
@@ -1,6 +1,19 @@
 #ifndef _SESSION_H_
 #define _SESSION_H_
+#include "common.h"
+typedef struct session
+{
+	int ctrl_fd;
+	//control conn
+	char cmdline[MAX_COMMAND_LINE];
+	char cmd[MAX_COMMAND];
+	char arg[MAX_ARG];
+	//fuzi tongdao
+	int parent_fd;
+	int child_fd;
+}session_t;
 
-void begin_session(int conn);
+
+void begin_session(session_t *sess);
 
 #endif /*_SESSION_H_*/
