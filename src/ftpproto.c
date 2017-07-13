9c6bc8606ef10c5ec3dd7462d6e4b6a718b44fc4
qinchao <qinchao252508@gmail.com>
2017-07-13 16:46:48 +0800

    add head file

--- src/ftpproto.c
+++ src/ftpproto.c
@@ -1,5 +1,18 @@
 #include "ftpproto.h"
-
+#include "sysutil.h"
 void handle_child(session_t *sess)
 {
+	writen(sess->ctrl_fd, "220 miniftpd 0.1\r\n", strlen("220 miniftpd 0.1\r\n"));
+
+	while(1)
+	{
+		memset(sess->cmdline, 0, sizeof(sess->cmdline));
+		memset(sess->cmd, 0, sizeof(sess->cmd));
+		memset(sess->arg, 0, sizeof(sess->arg));
+		readline(sess->ctrl_fd, sess->cmdline, MAX_COMMAND_LINE);
+
+		// find ftp command and args
+		//process command
+
+	}
 }
