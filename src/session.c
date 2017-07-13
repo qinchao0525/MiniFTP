4c6d0d03f13c93655cffce90ff5c14af89445be0
qinchao <qinchao252508@gmail.com>
2017-07-13 16:19:46 +0800

    add some using of session

--- src/session.c
+++ src/session.c
@@ -1,5 +1,28 @@
+#include "common.h"
 #include "session.h"
 
-void begin_session(int conn)
+void begin_session(session_t *sess)
 {
+	int sockfds[2];
+	if(socketpair(PF_UNIX, SOCK_STREAM, 0, sockfds)<0)
+		ERR_EXIT("socketpair");
+		
+
+	pid_t pid;
+	pid=fork();
+	if(pid<0)
+		ERR_EXIT("fork");
+	if(pid==0)
+	{
+		//ftp service process
+		close(sockfds[0]);
+		sess->child_fd=sockfds[1];
+		handle_child(sess);
+	}
+	else//nobody process
+	{
+		close(sockfds[1]);
+		sess->parent_fd=sockfds[0];
+		handle_parent(sess);
+	}
 }
