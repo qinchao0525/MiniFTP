bd518d9fe1c974c41f0100c0d3a0442d04ce7bad
qinchao <qinchao252508@gmail.com>
2017-07-21 10:30:05 +0800

    change a function defination error.

--- src/sysutil.h
+++ src/sysutil.h
@@ -17,7 +17,7 @@ int connect_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds)
 ssize_t readn(int fd, void*buf, size_t count);
 ssize_t writen(int fd, const void*buf, size_t count);
 ssize_t recv_peek(int sockfd, void* buf, size_t len);
-ssize_t recv_peek(int sockfd, void* buf, size_t len);
+ssize_t readline(int sockfd, void* buf, size_t maxline);
 
 void send_fd(int sock_fd, int fd);
 int recv_fd(const int sock_fd);
