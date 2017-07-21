1ae222689da54c4b351995ae14380eb372216f0a
qinchao <qinchao252508@gmail.com>
2017-07-21 15:20:39 +0800

    implement str functions.

--- src/str.c
+++ src/str.c
@@ -24,20 +24,67 @@ void str_split(const char *str, char* left, char* right, char c)
 
 int str_all_space(const char *str)
 {
-	return 0;
+	while(*str)
+	{
+		if(!isspace(*str))
+			return 0;
+		str++;
+	}
+	return 1;
 }
 
 void str_upper(char* str)
 {
+	while(*str)
+	{
+		*str=toupper(*str);
+		str++;
+	}
 }
 
 long long str_to_longlong(const char *str)
 {
-	return 0;
+	//return atoll(str);
+	if(str==NULL)
+		return 0;
+	long long result=0;
+	long long mult=1;
+	unsigned int len=strlen(str);
+	unsigned int i;//if i==0 and then i-1!=-1 it will be big again. This is a error will be show explicit.
+	if(len>15)
+		return 0;
+	for(i=0; i<len; i++)
+	{	
+		char ch=str[len-(i+1)];
+		long long val;
+		if(ch<'0' || ch>'9')
+			return 0;	
+		val=ch-'0';
+		val *=mult;
+		result += val;
+		mult *=10;
+	}
+	return result;
 }
 
 unsigned int str_octal_to_uint(const char *str)
 {
-	return 0;
+	unsigned int result=0;
+	int seen_non_zero=0;
+	while(*str)
+	{
+		int digit = *str;
+		if(!isdigit(digit)||digit>'7')
+			break;
+		if(digit!='0')
+			seen_non_zero=1;
+		if(seen_non_zero)
+		{
+			result << 3;
+			result += (digit-'0');
+		}
+		str++;
+	}
+	return result;
 }
 
