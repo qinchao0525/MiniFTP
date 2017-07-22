78138d4d7c9b1be8556e0b9d590abb07bb4f1436
qinchao <qinchao252508@gmail.com>
2017-07-22 11:34:04 +0800

    encapsulation.

--- src/ftpproto.c
+++ src/ftpproto.c
@@ -2,12 +2,95 @@
 #include "sysutil.h"
 #include "str.h"
 #include "ftpcodes.h"
-#include "common.h"
 
 void ftp_reply(session_t *sess, int status, const char* text);
+void ftp_lreply(session_t *sess, int status, const char* text);
 
 static void do_user(session_t *sess);
 static void do_pass(session_t *sess);
+static void do_cwd(session_t *sess);
+static void do_cdup(session_t *sess);
+static void do_quit(session_t *sess);
+static void do_port(session_t *sess);
+static void do_pasv(session_t *sess);
+static void do_type(session_t *sess);
+static void do_stru(session_t *sess);
+static void do_mode(session_t *sess);
+static void do_retr(session_t *sess);
+static void do_stor(session_t *sess);
+static void do_appe(session_t *sess);
+static void do_list(session_t *sess);
+static void do_nlst(session_t *sess);
+static void do_rest(session_t *sess);
+static void do_abor(session_t *sess);
+static void do_pwd(session_t *sess);
+static void do_mkd(session_t *sess);
+static void do_rmd(session_t *sess);
+static void do_dele(session_t *sess);
+static void do_rnfr(session_t *sess);
+static void do_rnto(session_t *sess);
+static void do_site(session_t *sess);
+static void do_syst(session_t *sess);
+static void do_feat(session_t *sess);
+static void do_size(session_t *sess);
+static void do_stat(session_t *sess);
+static void do_noop(session_t *sess);
+static void do_help(session_t *sess);
+
+typedef struct ftpcmd
+{
+	const char*cmd;
+	void (*cmd_handler)(session_t *sess);
+}ftpcmd_t; 
+
+static ftpcmd_t ctrl_cmds[]=
+{
+//conect control
+{"USER", do_user},
+{"PASS", do_pass},
+{"CWD", do_cwd},
+{"XCWD", do_cwd},
+{"CDUP", do_cdup},
+{"XCUP", do_cdup},
+{"QUIT", do_quit},
+{"ACCT", NULL},
+{"SMNT", NULL},
+{"REIN", NULL},
+//cmd arg
+{"PORT", do_port},
+{"PASV", do_pasv},
+{"TYPE", do_type},
+{"STRU", do_stru},
+{"MODE", do_mode},
+
+//service cmd
+{"RETR", do_retr},
+{"STOR", do_stor},
+{"APPE", do_appe},
+{"LIST", do_list},
+{"NLST", do_nlst},
+{"REST", do_rest},
+{"ABOR", do_abor},
+{"\377\364\377\362ABOR", do_abor},
+{"PWD", do_pwd},
+{"XPWD", do_pwd},
+{"MKD", do_mkd},
+{"XMKD", do_mkd},
+{"RMD", do_rmd},
+{"XRMD", do_rmd},
+{"DELE", do_dele},
+{"RNFR", do_rnfr},
+{"RNTO", do_rnto},
+{"SITE", do_site},
+{"SYST", do_syst},
+{"FEAT", do_feat},
+{"SIZE", do_size},
+{"STAT", do_stat},
+{"NOOP", do_noop},
+{"HELP", do_help},
+{"STOU", NULL},
+{"ALLO", NULL},
+};
 
 void handle_child(session_t *sess)
 {
@@ -31,10 +114,26 @@ void handle_child(session_t *sess)
 		str_split(sess->cmdline, sess->cmd, sess->arg, ' ');
 		printf("cmd=[%s], arg=[%s]\n", sess->cmd, sess->arg);
 		//process command
-		if(strcmp("USER", sess->cmd)==0)
+		/*if(strcmp("USER", sess->cmd)==0)
 			do_user(sess);
 		else if(strcmp("PASS", sess->cmd)==0)
-			do_pass(sess);
+			do_pass(sess);*/
+
+		int i=0;
+		int size=sizeof(ctrl_cmds)/sizeof(ctrl_cmds[0]);
+		for(i=0; i<size; i++)
+		{
+			if(strcmp(ctrl_cmds[i].cmd, sess->cmd)==0)
+			{
+				if(ctrl_cmds[i].cmd_handler != NULL)
+					ctrl_cmds[i].cmd_handler(sess);
+				else
+					ftp_reply(sess, FTP_COMMANDNOTIMPL, "unimplement command.");
+				break;
+			}
+		}
+		if(i == size)
+			ftp_reply(sess, FTP_BADCMD, "unknown command.");
 	}
 }
 
@@ -42,6 +141,13 @@ void ftp_reply(session_t *sess, int status, const char* text)
 {
 	char buf[1024]={0};
 	sprintf(buf, "%d %s\r\n", status, text);
+	writen(sess->ctrl_fd, buf, strlen(buf));
+}
+
+void ftp_lreply(session_t *sess, int status, const char* text)
+{
+	char buf[1024]={0};
+	sprintf(buf, "%d-%s\r\n", status, text);
 	writen(sess->ctrl_fd, buf, strlen(buf));
 }
 
@@ -95,3 +201,116 @@ static void do_pass(session_t *sess)
 
 	ftp_reply(sess, FTP_LOGINOK, "Login successful.");
 }
+
+static void do_cwd(session_t *sess)
+{
+}
+static void do_cdup(session_t *sess)
+{
+}
+static void do_quit(session_t *sess)
+{
+}
+static void do_port(session_t *sess)
+{
+}
+static void do_pasv(session_t *sess)
+{
+}
+static void do_type(session_t *sess)
+{
+	if(strcmp(sess->arg, "A")==0)
+	{
+		ftp_reply(sess, FTP_TYPEOK, "Switching to ASCII mode.");
+	}
+	else if(strcmp(sess->arg, "I")==0)
+	{
+		ftp_reply(sess, FTP_TYPEOK, "Switching to Binary mode.");
+	}
+	else
+		ftp_reply(sess, FTP_BADCMD, "Switching to Type mode.");
+}
+static void do_stru(session_t *sess)
+{
+}
+static void do_mode(session_t *sess)
+{
+}
+static void do_retr(session_t *sess)
+{
+}
+static void do_stor(session_t *sess)
+{
+}
+static void do_appe(session_t *sess)
+{
+}
+static void do_list(session_t *sess)
+{
+}
+static void do_nlst(session_t *sess)
+{
+}
+static void do_rest(session_t *sess)
+{
+}
+static void do_abor(session_t *sess)
+{
+}
+static void do_pwd(session_t *sess)
+{
+	char text[1024]={0};
+	char dir[1024+1]={0};
+	getcwd(dir, 1024);
+	sprintf(text, "\"%s\"", dir);
+	ftp_reply(sess, FTP_PWDOK, text);
+}
+static void do_mkd(session_t *sess)
+{
+}
+static void do_rmd(session_t *sess)
+{
+}
+static void do_dele(session_t *sess)
+{
+}
+static void do_rnfr(session_t *sess)
+{
+}
+static void do_rnto(session_t *sess)
+{
+}
+static void do_site(session_t *sess)
+{
+}
+static void do_syst(session_t *sess)
+{
+	ftp_reply(sess, FTP_SYSTOK, "UNIX type: L8");
+}
+static void do_feat(session_t *sess)
+{
+	ftp_lreply(sess, FTP_FEAT, "Features:");
+	writen(sess->ctrl_fd, " EPRT\r\n", strlen(" EPRT\r\n"));	
+	writen(sess->ctrl_fd, " EPSV\r\n", strlen(" EPSV\r\n"));	
+	writen(sess->ctrl_fd, " MDTM\r\n", strlen(" MDTM\r\n"));	
+	writen(sess->ctrl_fd, " PASV\r\n", strlen(" PASV\r\n"));	
+	writen(sess->ctrl_fd, " REST STREAM\r\n", strlen(" REST STREAM\r\n"));	
+	writen(sess->ctrl_fd, " SIZE\r\n", strlen(" SIZE\r\n"));	
+	writen(sess->ctrl_fd, " TUFS\r\n", strlen(" TUFS\r\n"));	
+	writen(sess->ctrl_fd, " UTF8\r\n", strlen(" UTF8\r\n"));
+	ftp_reply(sess, FTP_FEAT, "END");	
+}
+
+static void do_size(session_t *sess)
+{
+}
+static void do_stat(session_t *sess)
+{
+}
+static void do_noop(session_t *sess)
+{
+}
+static void do_help(session_t *sess)
+{
+}
+
