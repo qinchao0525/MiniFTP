1e2af6eb9b18b50ea60314119f2f6207c3dad237
qinchao <qinchao252508@gmail.com>
2017-07-23 11:41:30 +0800

    add function tcp_client implement

--- src/sysutil.c
+++ src/sysutil.c
@@ -1,5 +1,35 @@
 #include "sysutil.h"
 
+int tcp_client(unsigned short port)
+{
+	int sock;
+	if( (sock=socket(PF_INET, SOCK_STREAM, 0)) < 0 )
+	{
+		ERR_EXIT("tcp_client");
+	}
+	//bind 20 port
+	if(port > 0)
+	{
+		int on=1;
+		if( setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&on, sizeof(on)) < 0)
+		{	
+			ERR_EXIT("gethostbyname");
+		}
+
+		char ip[16]={0};
+		getlocalip(ip);
+		struct sockaddr_in localaddr;
+		memset(&localaddr, 0, sizeof(localaddr));
+		localaddr.sin_family = AF_INET;
+		localaddr.sin_port = htons(port);
+		localaddr.sin_addr.s_addr = inet_addr(ip);
+
+		if( (bind(sock, (struct sockaddr*)&localaddr, sizeof(localaddr)))<0 )
+			ERR_EXIT("Bind");
+	}
+	return sock;
+}
+
 //start tcp server.
 int tcp_server(const char* host, unsigned short port)
 {
