aa4a763cf82595d77f8b9303806b83c331fa106b
qinchao <qinchao252508@gmail.com>
2017-07-24 15:50:53 +0800

    fucntion in session.c use another implement function.

--- src/session.c
+++ src/session.c
@@ -2,16 +2,17 @@
 #include "session.h"
 #include "ftpproto.h"
 #include "privparent.h"
+#include "privsock.h"
 #include <pwd.h>
 
 void begin_session(session_t *sess)
 {
 
 	//unix domain connection
-	int sockfds[2];
+	/*int sockfds[2];
 	if(socketpair(PF_UNIX, SOCK_STREAM, 0, sockfds)<0)
 		ERR_EXIT("socketpair");
-		
+	*/	
 
 	pid_t pid;
 	pid=fork();
@@ -20,27 +21,19 @@ void begin_session(session_t *sess)
 	if(pid==0)
 	{
 		//ftp service process
-		close(sockfds[0]);
-		sess->child_fd=sockfds[1];
+		/*close(sockfds[0]);
+		sess->child_fd=sockfds[1];*/
+		priv_sock_set_child_context(sess);
 		handle_child(sess);
 	}
 	else//nobody process
 	{
 		
 		//change parent process to nobody process.
-		struct passwd *pw=getpwnam("nobody");	
-		if(pw==NULL)
-			return;
-		if( setegid(pw->pw_gid)<0 )
-		{
-			ERR_EXIT("setegid");
-		}
-		if( seteuid(pw->pw_uid)<0 )
-		{
-			ERR_EXIT("seteuid");
-		}
+		priv_sock_set_parent_context(sess);
+		/*
 		close(sockfds[1]);
-		sess->parent_fd=sockfds[0];
+		sess->parent_fd=sockfds[0];*/
 		handle_parent(sess);
 	}
 }
