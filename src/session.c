46c9a191e86ef49671ca252f15de6266970093a4
qinchao <qinchao252508@gmail.com>
2017-09-20 19:40:06 +0800

    add init and fd sending

--- src/session.c
+++ src/session.c
@@ -13,6 +13,7 @@ void begin_session(session_t *sess)
 	if(socketpair(PF_UNIX, SOCK_STREAM, 0, sockfds)<0)
 		ERR_EXIT("socketpair");
 	*/	
+	priv_sock_init(sess);
 
 	pid_t pid;
 	pid=fork();
@@ -24,6 +25,7 @@ void begin_session(session_t *sess)
 		/*close(sockfds[0]);
 		sess->child_fd=sockfds[1];*/
 		priv_sock_set_child_context(sess);
+		sess->ctrl_fd=priv_sock_recv_fd(sess->child_fd);
 		handle_child(sess);
 	}
 	else//nobody process
@@ -31,6 +33,7 @@ void begin_session(session_t *sess)
 		
 		//change parent process to nobody process.
 		priv_sock_set_parent_context(sess);
+		priv_sock_send_fd(sess->parent_fd, sess->p_ctrl_fd);
 		/*
 		close(sockfds[1]);
 		sess->parent_fd=sockfds[0];*/
